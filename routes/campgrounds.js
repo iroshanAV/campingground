5d98d78e2effa75518ab97ddd56b3940220302ad
iroshanAV <iroshanav@gmail.com>
2017-09-10 16:26:08 +0530

    published by part added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -16,17 +16,23 @@ router.get("/campgrounds", function(req,res){
     });
     
     
-    router.post("/campgrounds",function(req,res){
+    router.post("/campgrounds",isLoggedIn, function(req,res){
       var name = req.body.name;
       var image = req.body.image;
       var desc = req.body.description;
-      var newCampGround = {name:name,image:image,description:desc}
+      var author = {
+        id: req.user._id,
+        username : req.user.username
+      };
+      var newCampGround = {name:name,image:image,description:desc,author:author}
+      console.log(req.user);
       //create a new campground and save to DB
       Campground.create(newCampGround, function(err,newlyCreated){
        if(err){
          console.log(err);
        }else{
           //redirect back to campgrounds page
+          console.log("assasasasasasa"+newlyCreated);
          res.redirect("/campgrounds");
        }
       });
@@ -37,7 +43,7 @@ router.get("/campgrounds", function(req,res){
     
     
     
-    router.get("/campgrounds/new",function(req,res){
+    router.get("/campgrounds/new",isLoggedIn, function(req,res){
     res.render("campgrounds/new");
     });
     
@@ -57,5 +63,14 @@ router.get("/campgrounds", function(req,res){
     
     });
     
+
+    function isLoggedIn(req,res,next){
+      if(req.isAuthenticated()){
+      return next();
+      }
+      res.redirect("/login");
+      }
+      
+
 
     module.exports = router;
